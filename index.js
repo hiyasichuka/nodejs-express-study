10d4cf44858420d40d3ad35d2e1bc9d8512ea6d2
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2021-06-24 10:44:29 +0900

    modified

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 var os = require('os');
 console.log(os.platform());
+console.log(process.cwd());
+
+let path = require("path");
+let s = process.argv[1];
+// process.chdir(path.dirname(process.argv[1]));
+process.chdir(__dirname);
+console.log(process.cwd());
