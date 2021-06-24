43740a4dc3a2add574b0c398c852f407eb5e6b33
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2021-06-24 11:02:30 +0900

    EventEmitter

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-var os = require('os');
-console.log(os.platform());
-console.log(process.cwd());
+let EventEmitter = require("events").EventEmitter;
+let util = require("util");
 
-let path = require("path");
-let s = process.argv[1];
-// process.chdir(path.dirname(process.argv[1]));
-process.chdir(__dirname);
-console.log(process.cwd());
+let Animal = function () {
+
+  EventEmitter.call(this);
+};
+
+util.inherits(Animal, EventEmitter);
+
+
+Animal.prototype.message = "qwert";
+
+Animal.prototype.say = function () {
+  console.log(this.message);
+  this.emit("say", this.message);
+};
+
+
+let Dog = function () {
+  Animal.call(this);
+  this.message = "wanwan"
+};
+
+util.inherits(Dog, Animal);
+
+let dog = new Dog();
+dog.on("say", function (message) {
+  console.log(`${message}と叫んだ`);
+});
+dog.say();
