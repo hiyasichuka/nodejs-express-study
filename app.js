2f7fcee7a04d23147f8c9814e115733b85251347
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2021-06-25 16:37:51 +0900

    cookie

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 var express = require("express");
-var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
 var app = express();
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json());
+app.use(cookieParser());
 
-app.get("/",(req,res)=>{
-  res.render("./index.ejs");
+app.get("/", (req, res) => {
+  var count = parseInt(req.cookies.count || 0);
+  res.cookie("count", count + 1);
+  res.render("./index.ejs", { count });
 });
-
-app.post("/",(req,res)=>{
-  res.send("OK");
-});
-
-app.use("/public",express.static(__dirname + "/public"));
 
 app.listen(8080);
