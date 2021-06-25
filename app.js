0dffa9b9875a46d69d794b227f1d926860bac110
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2021-06-25 16:15:05 +0900

    body-parser

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
+
+app.set("view engine","ejs");
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+app.get("/",(req,res)=>{
+  res.render("./index.ejs");
+});
+
+app.post("/",(req,res)=>{
+  res.send("OK");
+});
 
 app.use("/public",express.static(__dirname + "/public"));
 
