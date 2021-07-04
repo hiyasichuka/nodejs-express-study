e6873de69361feaa442c18dd8c81d02b4acdec6a
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2021-07-05 08:36:07 +0900

    ejsテンプレ

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log(process.env.NODE_ENV);
+var express = require('express'),
+    app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// middleware
+//app.use(express.logger('dev'));
+//app.use(app.router);
+//app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.render('index', {title: 'ページタイトル'});
+});
+
+app.listen(3000);
+console.log("server starting...");
