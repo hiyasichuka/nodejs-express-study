1bb8e994b62b6a60b662a36548a1571209f7a85c
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2021-07-26 16:02:07 +0900

    expressとejsを追加してサンプルページを作成

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+var express = require("express"),
+    app = express();
+
+app.set("view engine","ejs");
+
+app.get("/",(req,res)=>{
+  res.render("./index.ejs");
+});
+
+app.listen(8080);
 console.log(process.env.NODE_ENV);
