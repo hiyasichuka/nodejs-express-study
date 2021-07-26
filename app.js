bc1c78fbd97157663bd199159e37cecbfb9611b7
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2021-07-26 16:24:36 +0900

    環境変数による切り替え処理追加

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ app.set("view engine", "ejs");
 
 app.disable("x-powered-by");
 
-//静的ファイルの配信
-app.use("/public", express.static(__dirname+"/public"));
+//静的ファイルの配信(prod,devで切り替え）
+app.use("/public", express.static(__dirname + "/public/" + process.env.NODE_ENV === "dev" ? "dev" : "prod"));
 
 // ルート設定
-app.use("/",require("./routes/index.js"));
+app.use("/", require("./routes/index.js"));
 
 app.get("/", (req, res) => {
   res.render("./index.ejs");
