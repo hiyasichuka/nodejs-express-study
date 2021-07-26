2f5ed607a03359e3e626f9de2081b70278cbf4af
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2021-07-26 16:11:27 +0900

    router処理追加

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ app.set("view engine", "ejs");
 
 app.disable("x-powered-by");
 
+//静的ファイルの配信
+app.use("/public", express.static(__dirname+"/public"));
+
+// ルート設定
+app.use("/",require("./routes/index.js"));
+
 app.get("/", (req, res) => {
   res.render("./index.ejs");
 });
